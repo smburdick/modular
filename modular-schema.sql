4a92e86707be7f0b41e7925400e2c3bd49ae8960
presleyoreed3 <presleyoreed3@gmail.com>
2017-11-12 14:23:45 -0800

    made User relation in schema

--- modular-schema.sql
+++ modular-schema.sql
@@ -2,3 +2,15 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+CREATE TABLE User(
+	user_id INTEGER PRIMARY KEY NOT NULL,
+	username TEXT NOT NULL,
+	f_name TEXT NOT NULL,
+	l_name TEXT NOT NULL,
+	birth_day INTEGER,
+	birth_month INTEGER,
+	birth_year INTEGER,
+	bio TEXT,
+	hashed_password TEXT
+)
