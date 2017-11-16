a8b393b747f0387020a587d5e3053d42ffa23f85
Sam Burdick <smburdick@pugetsound.edu>
2017-11-15 17:05:01 -0800

    updated types, added billing info, having syntax issues

--- ddl/modular-schema.sql
+++ ddl/modular-schema.sql
@@ -16,15 +16,18 @@ CREATE TABLE User(
 );
 
 CREATE TABLE Model(
-	model_id INTEGER PRIMARY KEY,
+	model_id INTEGER,
 	creator_id INTEGER,
 	material TEXT,
-	cost TEXT,
+	cost INTEGER,
 	object_file TEXT,
 	parent_id INTEGER,
+	PRIMARY KEY(model_id),
 	FOREIGN KEY creator_id REFERENCES User(user_id) 
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+	FOREIGN KEY parent_id REFERENCES Model(model_id)
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE Created(
@@ -34,11 +37,11 @@ CREATE TABLE Created(
 	uploaded_date TEXT,
 	PRIMARY KEY (model_id, user_id),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE Review(
@@ -46,14 +49,14 @@ CREATE TABLE Review(
 	model_id INTEGER,
 	review_date TEXT NOT NULL,
 	comment TEXT NOT NULL,
-	stars TEXT NOT NULL,
+	stars INTEGER CHECK stars > 0 AND stars < 6,
 	PRIMARY KEY (model_id, user_id),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE Bookmarks(
@@ -61,11 +64,11 @@ CREATE TABLE Bookmarks(
 	model_id INTEGER,
 	PRIMARY KEY (model_id, user_id),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE Purchases(
@@ -75,26 +78,41 @@ CREATE TABLE Purchases(
 	quantity INTEGER CHECK quantity > 0,
 	PRIMARY KEY (model_id, user_id, purchase_date),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE,
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE Address(
 	user_id INTEGER,
-	address_id TEXT,
-	address_one TEXT,
-	address_two TEXT,
+	address_id INTEGER,
+	address_line_one TEXT NOT NULL,
+	address_line_two TEXT NOT NULL,
 	city TEXT,
 	state TEXT,
-	zipcode INTEGER,
+	zipcode INTEGER NOT NULL,
 	country TEXT,
 	PRIMARY KEY (user_id, address_id),
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+);
+
+CREATE TABLE BankingInfo (
+	banking_info_id INTEGER,
+	user_id INTEGER,
+	card_number INTEGER NOT NULL,
+	ccv INTEGER NOT NULL,
+	billing_address_id INTEGER NOT NULL,
+	name_on_card TEXT NOT NULL,
+	PRIMARY KEY (banking_info_id),
+	FOREIGN KEY (user_id) REFERENCES User(user_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (billing_address_id) REFERENCES Address(address_id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
 );
 
 CREATE TABLE InCart(
@@ -103,11 +121,10 @@ CREATE TABLE InCart(
 	quantity INTEGER CHECK quantity > 0,
 	PRIMARY KEY (model_id, user_id),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE,
 	FOREIGN KEY user_id REFERENCES User(user_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE BelongsTo(
@@ -115,11 +132,10 @@ CREATE TABLE BelongsTo(
 	category_id INTEGER,
 	PRIMARY KEY (model_id, category_id),
 	FOREIGN KEY model_id REFERENCES Model(model_id)
-		ON UPDATE cascade
-		ON DELETE cascade,
+		ON UPDATE CASCADE
 	FOREIGN KEY category_id REFERENCES Category(category_id)
-		ON UPDATE cascade
-		ON DELETE cascade
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
 CREATE TABLE Category(
