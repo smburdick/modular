56d2b0d3c58638cb77cc45fe8fc75f0d441a9f58
Sam Burdick <smburdick@pugetsound.edu>
2017-12-02 22:29:08 -0800

    modified styling

--- pages/home/index.php
+++ pages/home/index.php
@@ -1,72 +1,19 @@
-<!-- Source: https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_temp_webpage&stacked=h -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Modular</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link rel = "stylesheet"
-   type = "text/css"
-   href = "css/style.css" /> 
-</head>
+<?php
+  include '../boilerplate.php';
+  generate_head('Modular', '');
+?>
 <body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <div class="navbar-brand">Modular</div> <!-- TODO logo -->
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="cart/index.php">Cart</a></li>
-        <li><a href="#">Contact</a></li>
-        <li>
-        <form class="navbar-form" role="search" method="get" action="search/search_results.php">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        </div>
-        </form>
-      </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<!--
-<div class="container-fluid text-center">    
+<div class="container-fluid text-center"> 
   <div class="row content">
-    <div class="col-sm-2 sidenav">
 
+    <div class="col-sm-2 sidenav">
     </div>
     <div class="col-sm-8 text-left"> 
-
-      <h1>Welcome</h1>
-      <p>Scroll through featured products here.</p>
-      <hr>
-      <h3>Test</h3>
-      <p>Featured categories here.</p>
-    </div>
-
+    <br><br><center><h1>Welcome to Modular</h1></center>
     <div class="col-sm-2 sidenav">
     </div>
-  </div>
-</div>
--->
 
 </body>
 </html>
