92878f871c43aae9d27cb0787b00abad4287c7ee
presleyoreed3 <presleyoreed3@gmail.com>
2017-12-02 21:10:05 -0800

    fixed month year mixup in BankingInfo tabel

--- pages/profile/addCredit.php
+++ pages/profile/addCredit.php
@@ -1,18 +1,14 @@
 <?php	
 	echo '<h2>Please add your Credit Card Info Below</h2>
-	<form action="inputAddress.php" method="post">
+	<form action="inputCredit.php" method="post">
+		Name on Card:<br>
+		<input type="text" name="name_on_card"><span class="error"> * </span><br>
 		Card Number:<br>
 		<input type="text" name="card_number"><span class="error"> * </span><br>
-		:<br>
-		<input type="text" name="address_line_two"><span class="error"> * </span><br>
-		City: <br>
-		<input type="text" name="city"><span class="error"> * </span><br>
-		State: <br>
-		<input type="text" name="state"><span class="error"> * </span><br>
-		Zipcode: <br>
-		<input type="text" name="zipcode"><span class="error"> * </span><br>
-		Country: <br>
-		<input type="text" name="country"><span class="error"> * </span><br><br>
+		Expiration Date: <br>
+		<input type="text" name="expiration_day">	<input type="text" name="expiration_month"><br>
+		CCV: <br>
+		<input type="text" name="state"><span class="error"> * </span><br><br>
 		<input type="submit" value="Submit">
 	</form>';
 ?>
