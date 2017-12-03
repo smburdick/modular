be9a90b5a62c23bde4123d307ca2f799c89a02ef
Ian <iwwhite@pugetsound.edu>
2017-12-02 17:36:11 -0800

    modified ian-test.sql

--- test/ian-test.sql
+++ test/ian-test.sql
@@ -65,13 +65,13 @@ CREATE TABLE User(
 insert into Material values (0, "wood", 999);
 insert into Color values ('#FFFFFF', 'white');
 insert into User values (0, "ian", "ian", "white", null ,null, null, null, null, null, null);
-insert into Model values (0,0,0,100,'#FFFFFF', null, 0, "cube", 0);
+insert into Model values (0,0,0,100,'#FFFFFF', null, 0, "cube", 0, null, null);
 INSERT INTO Category VALUES (
 	0,
 	"zun",
 	"just a whole lotta zun stuff my dude"
 	);
-insert into belongsto values (0, 0);
+
 
 insert into User values (1, "bill", "bill", "chill", null ,null, null, null, null, null, null);
 insert into User values (2, "douglas", "douglas", "mcarthur", null ,null, null, null, null, null, null);
@@ -81,10 +81,21 @@ insert into Material values (1, "metal", 37);
 insert into Material values (2, "plastic", 97);
 insert into Material values (3, "water", 9);
 
-insert into Model values (1,1,1,100,'#FFFFFF', null, 0, "ship", 0);
-insert into Model values (2,2,2,100,'#FFFFFF', null, 0, "skateboard", 0);
-insert into Model values (3,3,3,100,'#FFFFFF', null, 0, "chair", 0);
+insert into Model values (1,1,1,100,'#FFFFFF', null, 0, "ship", 0, null, null);
+insert into Model values (2,2,2,100,'#FFFFFF', null, 0, "skateboard", 0, null, null);
+insert into Model values (3,3,3,100,'#FFFFFF', null, 0, "chair", 0, null, null);
 
+
+
+insert into Model values (4,1,1,100,'#FFFFFF', null, 0, "ship", 0, null, null);
+insert into Model values (5,2,2,100,'#FFFFFF', null, 0, "skateboard", 0, null, null);
+insert into Model values (6,3,3,100,'#FFFFFF', null, 0, "chair", 0, null, null);
+
+insert into Model values (7,1,1,100,'#FFFFFF', null, 0, "building", 0, null, null);
+insert into Model values (8,2,2,100,'#FFFFFF', null, 0, "car", 0, null, null);
+insert into Model values (9,3,3,100,'#FFFFFF', null, 0, "table", 0, null, null);
+
+insert into belongsto values (0, 0);
 insert into belongsto values (1, 1);
 insert into belongsto values (2, 2);
 insert into belongsto values (3, 3);
@@ -96,11 +107,3 @@ insert into belongsto values (6, 3);
 insert into belongsto values (7, 1);
 insert into belongsto values (8, 2);
 insert into belongsto values (9, 3);
-
-insert into Model values (4,1,1,100,'#FFFFFF', null, 0, "ship", 0);
-insert into Model values (5,2,2,100,'#FFFFFF', null, 0, "skateboard", 0);
-insert into Model values (6,3,3,100,'#FFFFFF', null, 0, "chair", 0);
-
-insert into Model values (7,1,1,100,'#FFFFFF', null, 0, "building", 0);
-insert into Model values (8,2,2,100,'#FFFFFF', null, 0, "car", 0);
-insert into Model values (9,3,3,100,'#FFFFFF', null, 0, "table", 0);
