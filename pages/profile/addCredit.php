93dcce43176de2d4faab54ce6ac0361c3055d7b4
presleyoreed3 <presleyoreed3@gmail.com>
2017-12-03 18:25:02 -0800

    added styling to addCredit.php

--- pages/profile/addCredit.php
+++ pages/profile/addCredit.php
@@ -1,14 +1,27 @@
-<?php	
-	echo '<h2>Please add your Credit Card Info Below</h2>
-	<form action="inputCredit.php" method="post">
-		Name on Card:<br>
-		<input type="text" name="name_on_card"><span class="error"> * </span><br>
-		Card Number:<br>
-		<input type="text" name="card_number"><span class="error"> * </span><br>
-		Expiration Date (MM/YY): <br>
-		<input type="text" name="expiration_month">	<input type="text" name="expiration_year"><span class="error"> * </span><br>
-		CCV: <br>
-		<input type="text" name="ccv"><span class="error"> * </span><br><br>
-		<input type="submit" value="Submit">
-	</form>';
+<?php
+ 	include '../boilerplate.php';
+    generate_head('Editor', '');
+
+echo '<div class="container-fluid text-center">    
+	<div class="row content">
+		<div class="col-sm-2 sidenav">
+		</div>
+		<div class="col-sm-8 text-left"> 
+		<h2>Please add your Credit Card Info Below</h2>
+			<form action="inputCredit.php" method="post">
+				Name on Card:<br>
+				<input type="text" name="name_on_card"><span class="error"> * </span><br>
+				Card Number:<br>
+				<input type="text" name="card_number"><span class="error"> * </span><br>
+				Expiration Date (MM/YY): <br>
+				<input type="text" name="expiration_month">	<input type="text" name="expiration_year"><span class="error"> * </span><br>
+				CCV: <br>
+				<input type="text" name="ccv"><span class="error"> * </span><br><br>
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+		<div class="col-sm-2 sidenav">
+	</div>
+</div>';
+
 ?>
