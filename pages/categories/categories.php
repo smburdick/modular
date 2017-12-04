d3e21c9b2dc811bf1c0c0226ac8d9af0d0c42873
gpinkard <gpinkard@pugetsound.edu>
2017-12-03 18:25:18 -0800

    new prod page + cat pages. Prettier.

--- pages/categories/categories.php
+++ pages/categories/categories.php
@@ -1,86 +1,42 @@
-
-<html lang="en">
-<head>
-  <title>Modular</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel = "stylesheet"
-  type = "text/css"
-  href = "../css/style.css" />
-  <!-- TODO move to separate file -->
-</head>
-<body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Modular</a> <!-- TODO logo -->
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Cart</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <!-- TODO this should be conditioned on whether the user is logged in. We'll need to set a cookie to check this. -->
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav> 
-<div class="container-fluid text-center">    
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-    </div>
-    <div class="col-sm-8 text-left"> 
-      <h1>Categories</h1>
-	<?php
-	// REMEMBER TO CHANGE DB PATH WHEN YOU MOVE OVER BACK TO SERVER
-	  $db_path = '../../db/modular.db';
-	  try {
-	    $db = new PDO('sqlite:' . $db_path);
-	    $get_categories = 'select * from category;';
-	    $result_set = $db->query($get_categories);
-	    //loop and print out all the categories
-	    foreach($result_set as $tuple){
-	      $name = $tuple["category_name"];
-	      $description = $tuple["category_description"];
-	      $categoryID = $tuple["category_id"];
-	      echo "<div class='card'>";
-	      echo "<div class='card-header'>";
-	      echo "<a href='/categories/selected_category.php?id=$categoryID'>$name</a>";
-	      echo "<div class='card-body'>";
-	      echo "<i>$tuple[category_description]</i>";
-	      echo "</div>";
-	      echo "</div>";
-	      echo "</div>";
-	      echo "<br>";
-	    }
-	  } catch(PDOException $e){
-	    die('Exception : ' . $e->getMessage());
-	  }
-	  $db = null // disconnect 
-	?>
-	<br>
-      <hr>
-    </div>
-    <div class="col-sm-2 sidenav">
-    </div>
-  </div>
-</div>
-
-<footer class="container-fluid text-center">
-  <p align="left">2017 Modular</p>
-</footer>
-
-</body>
-</html>
+  <?php
+    //Gabriel Pinkard
+    echo "<!DOCTYPE html>";
+    echo "<html lang='en'>";
+    include '../boilerplate.php';
+    generate_head('Categories', 'Categories');
+    echo "<div class='container-fluid text-center'>";
+    echo "<div class='row content'>";
+    echo "<div class='col-sm-3 sidenav'>";
+    echo "</div>";
+    echo "<div class='col-sm-6 text-center'>";
+    echo "<h1>Categories</h1>";
+    echo "<br>";
+    $db_path = '../../db/modular.db';
+      try {
+	$db = new PDO('sqlite:' . $db_path);
+	$get_categories = 'select * from category;';
+	$result_set = $db->query($get_categories);
+	//loop and print out all the categories
+	foreach($result_set as $tuple){
+	  $name = $tuple["category_name"];
+	  $description = $tuple["category_description"];
+	  $categoryID = $tuple["category_id"];
+	  echo "<div class='card'>";
+	  echo "<div class='card-header'>";
+	  echo "<a href='/categories/selected_category.php?id=$categoryID&cat_name=$name'><font size='5'>$name</font></a>";
+	  echo "<div class='card-body'>";
+	  echo "<i>$tuple[category_description]</i>";
+	  echo "</div>";
+	  echo "</div>";
+	  echo "</div>";
+	  echo "<br>";
+	}
+      } catch(PDOException $e){
+	die('Exception : ' . $e->getMessage());
+      }
+    $db = null; // disconnect 
+    echo "<div class='col-sm-3 sidenav'>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+  ?>
--- pages/product/addedToCart.php
+++ pages/product/addedToCart.php
@@ -1,47 +1,9 @@
-
-<html lang="en">
-<head>
-  <title>Modular</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <!-- TODO move to separate file -->
-  <link rel = "stylesheet"
-  type = "text/css"
-  href = "../css/style.css" /> 
-</head>
-<body>
-
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Cart</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <!-- TODO this should be conditioned on whether the user is logged in. We'll need to set a cookie to check this. -->
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav> 
-<div class="container-fluid text-center" style="height: 450px">    
-  <div class="row content">
-    <div class="col-sm-1 sidenav">
-    </div>
 <?php 
   // Gabriel Pinkard
+  echo "<!DOCTYPE html>";
+  echo "<html lang='en'>";
+  include '../boilerplate.php';
+  generate_head('Categories', 'Categories');
   $success = false;
   $results = false;
   $db_path = '../../db/modular.db'; 
@@ -77,12 +39,15 @@
   }
   echo "<br>";
   if($success || $result){
-    echo "<p>Added $quantity $model_name model to your cart</p>";
+    echo "<div class='col-sm-8 text-center'>"; 
+    echo "<font size='6' color='282a2e'> Added $quantity $model_name model(s) to your cart.</font>";
+    echo "</div>";
+    //echo ""; // echo photo of item
   } else {
-    echo "There was an error processing your request";
+    echo "<div class='col-sm-8 text-center'>"; 
+    echo "<font size='6' color='282a2e'> There was an error processing your request.</font>";
+    echo "</div>";
   }
   $db = null; // disconnect  
   //header("Location: /index.php"); // go back to product page
 ?>
-</body>
-</html>
